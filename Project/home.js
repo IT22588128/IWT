3424218b65030373c5745333cdf7f52c26f32d7e
IT22588128 <127617127+IT22588128@users.noreply.github.com>
2023-05-22 18:36:38 +0530

    2023/05/22
    2023/05/22

--- Project/home.js
+++ Project/home.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', function () {
     var slides = document.querySelectorAll('.slide');
     var currentSlide = 0;
     var totalSlides = slides.length;
-    var slideInterval = setInterval(nextSlide, 5000);
+    var slideInterval = setInterval(nextSlide,5000);
   
     function nextSlide() {
       slides[currentSlide].classList.remove('active');
@@ -15,11 +15,22 @@ window.addEventListener('DOMContentLoaded', function () {
 
 
 
+  function search_animal() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('l1');
+      
+    selectElement = document.querySelector('#city');
+    output = selectElement.options[selectElement.selectedIndex].value;
 
+    for (i = 0; i < x.length; i++) {
 
+        if (!x[i].innerHTML.toLowerCase().includes(input) || !x[i].innerHTML.toLowerCase().includes(output)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
 
-
-  
-
-
-
+    }
+}
